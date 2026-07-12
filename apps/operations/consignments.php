ae86dbe111018f893df42fb11d869ae255d6e614
victoriatoivo-hash <victoriatoivo-hash@users.noreply.github.com>
2026-07-12 21:31:30 +0200

    Refresh packing stylesheet cache

--- apps/operations/consignments.php
+++ apps/operations/consignments.php
@@ -19,7 +19,7 @@ $packingJsVersion = is_file(BASE_PATH . '/assets/js/packing-list.js')
     ? (string) filemtime(BASE_PATH . '/assets/js/packing-list.js') . '-visibility1'
     : (string) time();
 $packingCssVersion = is_file(BASE_PATH . '/assets/css/packing-board.css')
-    ? (string) filemtime(BASE_PATH . '/assets/css/packing-board.css') . '-v2'
+    ? (string) filemtime(BASE_PATH . '/assets/css/packing-board.css') . '-v3'
     : (string) time();
 $extraStylesheets[] = [
     'path' => 'assets/css/packing-board.css',
